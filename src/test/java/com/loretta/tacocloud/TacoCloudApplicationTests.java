3537f2eb8e265dc66f74845c4540877e3236a0cd
LorettaYoo <luoyutong598@163.com>
2023-02-17 17:24:46 +0800

    init: init taco-cloud

---
+++ src/test/java/com/loretta/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loretta.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
